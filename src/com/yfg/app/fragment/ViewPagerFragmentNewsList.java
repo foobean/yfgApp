e43ea9b312b65e392d913aad4ff99a6e98f1c2c0
andyuan1000 <1350250929@qq.com>
2013-04-18 16:25:48 +0800

    add

--- src/com/yfg/app/fragment/ViewPagerFragmentNewsList.java
+++ src/com/yfg/app/fragment/ViewPagerFragmentNewsList.java
@@ -23,12 +23,10 @@ import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class ViewPagerFragmentNewsList  extends ListFragment{
-
-
     private ListView listView  = null;
     // URL
   	//public static final String URL = "http://api.androidhive.info/music/music.xml";//xml
-  	public static final String URL = "http://192.168.1.105:8080/newslist/newslist.xml";//xml
+  	public String URL = "http://192.168.1.105:8080/newslist/newslist.action";//xml
     // XML 
   	public static final String KEY_NEWS = "news"; // parent node
   	public static final String KEY_ID = "id";
@@ -45,11 +43,20 @@ public class ViewPagerFragmentNewsList  extends ListFragment{
 	     return v;
 	}
 	
+	
+	public ViewPagerFragmentNewsList(){}
+	
+	
+	public ViewPagerFragmentNewsList(int position){
+		URL += "?id="+ String.valueOf(position);
+	}
+	
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		 super.onActivityCreated(savedInstanceState);
 		 listView = this.getListView();;
-	     ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+	     ArrayList<HashMap<String, String>> newsList = new ArrayList<HashMap<String, String>>();
          XMLParser parser = new XMLParser();
          String xml = parser.getXmlFromUrl(URL); // internet xml
          Document doc = parser.getDomElement(xml); // parser DOM 
@@ -66,9 +73,9 @@ public class ViewPagerFragmentNewsList  extends ListFragment{
                  map.put(KEY_COMMENTNUM, parser.getValue(e, KEY_COMMENTNUM));
                  map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
                  // HashList
-                 songsList.add(map);
+                 newsList.add(map);
          }
-         adapter=new LazyAdapter(getActivity(), songsList);        
+         adapter=new LazyAdapter(getActivity(), newsList);        
          listView.setAdapter(adapter);
          listView.setOnItemClickListener(new OnItemClickListener(){
 
@@ -76,7 +83,7 @@ public class ViewPagerFragmentNewsList  extends ListFragment{
 			public void onItemClick(AdapterView<?> parent, View view,
 	                int position, long id) {
 				
-				Log.i("println==================="," - position: "+Integer.toString(position));
+				Log.i(view+"println==================="," - position: "+Integer.toString(position)+id);
 			
 			}
         	 
---
+++ src/com/yfg/app/fragment/ViewPagerTitleFragment.java
@@ -0,0 +1,74 @@
+package com.yfg.app.fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.slidingmenu.lib2.SlidingMenu;
+import com.viewpagerindicator.TabPageIndicator;
+import com.yfg.app.activity.HomeActivity;
+import com.yfg.app.activity.R;
+import com.yfg.app.fragment.adapter.ViewPagerIndicatorAdpter;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ *  author:lyuan
+ * 
+ * */
+public class ViewPagerTitleFragment extends Fragment {
+
+	private HomeActivity mMain = null;
+	private TabPageIndicator indicator = null;
+	private View contextView = null;
+	private ViewPager pager = null;
+	private ViewPagerIndicatorAdpter myAdapter = null;
+	private List<ListFragment> fragmentList = new ArrayList<ListFragment>();
+	private List<String>   titleList    = new ArrayList<String>();
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        titleList.add("title 1 ");
+        titleList.add("title 2 ");
+        titleList.add("title 3 ");
+        titleList.add("title 4 ");
+        titleList.add("title 5 ");
+		myAdapter = new ViewPagerIndicatorAdpter(getFragmentManager(), fragmentList, titleList);
+		mMain = (HomeActivity) getActivity();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		contextView = inflater.inflate(R.layout.simple_tabs, container, false);
+		pager = (ViewPager) contextView.findViewById(R.id.pager);
+		
+		pager.setAdapter(myAdapter);
+		
+		indicator = (TabPageIndicator) contextView.findViewById(R.id.indicator);
+		indicator.setViewPager(pager);
+		indicator.setOnPageChangeListener(onPageChangeListener);
+		return contextView;
+	}
+
+	
+	ViewPager.SimpleOnPageChangeListener onPageChangeListener = new ViewPager.SimpleOnPageChangeListener(){
+		
+		@Override
+        public void onPageSelected(int position) {
+            switch (position) {
+                case 0:
+                	mMain. getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+                    break;
+                default:
+                	mMain. getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+                    break;
+            }
+        }
+	};
+	
+}
